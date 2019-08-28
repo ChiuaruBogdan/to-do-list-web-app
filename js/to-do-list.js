cb707dde109749f3ee8ef01d4bab572c15519402
Chiuaru Bogdan <bchiuaru94@gmail.com>
2019-08-28 20:52:43 +0300

    displaying items from API to web page

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,38 +1,68 @@
+window.ToDoList = {
+    API_BASE_URL: "http://localhost:8083/to-do-items",
 
-   window.ToDoList = {
-      API_BASE_URL: "http://localhost:8083/to-do-items",
+    createItem: function () {
+        var description = $("#description-field").val();
+        var deadline = $("#deadline-picker").val();
 
-       createItem: function () {
-          var description = $ ("#description-field").val();
-          var deadline = $ ("#deadline-picker").val();
+        // $$$ = simbol JQuery
 
-          // $$$ = simbol JQuery
+        var item = {
+            description: description,
+            deadline: deadline
+        };
 
-          var item = {
-              description: description,
-              deadline: deadline
-       };
+        $.ajax({
+            url: ToDoList.API_BASE_URL,
+            method: "POST",
+            //MIME type mai jos
+            contentType: "application/json",
+            data: JSON.stringify(item)
+        }).done(function (response) {
+            console.log("Successfully received response");
+            console.log(response)
+        })
+    },
 
-       $.ajax({
-           url: ToDoList.API_BASE_URL,
-           method: "POST",
-           //MIME type mai jos
-           contentType: "application/json",
-           data: JSON.stringify(item)
-       }).done(function (response) {
-           console.log("Successfully received response");
-           console.log(response)
-   })
-},
-       //mai jos e pentru evenimente, clickuri si toate cele
-       bindEvents: function () {
+    getItems: function () {
+        $.ajax({
+            url: ToDoList.API_BASE_URL,
+            method: "GET",
+        }).done(function (response) {
+            console.log("Successfully received response");
+            console.log(response);
+            ToDoList.displayItems(JSON.parse(response));
+        })
+    },
 
-          $('#new-item-form').submit(function (event) {
-              event.preventDefault();
+    displayItems: function (items){
+        var tableBodyHtml = '';
 
-              ToDoList.createItem();
-          });
-       }
+        items.forEach(item => tableBodyHtml += ToDoList.getItemRow(item));
+        $(`#to-do-items-table tbody`).html(tableBodyHtml);
+    },
+
+    getItemRow: function (item){
+        var formatedDate = new Date(...item.deadline).toLocaleDateString("en-US");
+
+      return `<tr>
+            <td>${item.description}</td>
+            <td>${formatedDate}</td>
+            <td><input type="checkbox" class="mark-done-checkbox" title="Completed"/></td>
+            <td><a href="#" class="delete-item fa fa-trash"></a></td>
+        </tr>`
+    },
+
+    //mai jos e pentru evenimente, clickuri si toate cele
+    bindEvents: function () {
+
+        $('#new-item-form').submit(function (event) {
+            event.preventDefault();
+
+            ToDoList.createItem();
+        });
+    }
 
 };
-   ToDoList.bindEvents();
+ToDoList.getItems()
+ToDoList.bindEvents();
