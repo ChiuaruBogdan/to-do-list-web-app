bfb9b9669f75bd7745ea432c551d9b33540756f0
Chiuaru Bogdan <bchiuaru94@gmail.com>
2019-09-04 19:49:02 +0300

    Displaying checkbox as marked

--- js/to-do-list.js
+++ js/to-do-list.js
@@ -1,11 +1,10 @@
+
 window.ToDoList = {
     API_BASE_URL: "http://localhost:8083/to-do-items",
 
     createItem: function () {
         var description = $("#description-field").val();
         var deadline = $("#deadline-picker").val();
-
-        // $$$ = simbol JQuery
 
         var item = {
             description: description,
@@ -15,12 +14,11 @@ window.ToDoList = {
         $.ajax({
             url: ToDoList.API_BASE_URL,
             method: "POST",
-            //MIME type mai jos
+            // MIME type
             contentType: "application/json",
             data: JSON.stringify(item)
         }).done(function (response) {
-            console.log("Successfully received response");
-            console.log(response)
+            ToDoList.getItems();
         })
     },
 
@@ -31,29 +29,57 @@ window.ToDoList = {
         }).done(function (response) {
             console.log("Successfully received response");
             console.log(response);
+
             ToDoList.displayItems(JSON.parse(response));
         })
     },
 
-    displayItems: function (items){
+    deleteItem: function (itemId) {
+        $.ajax({
+            url: ToDoList.API_BASE_URL + "?id=" + itemId,
+            method: "DELETE"
+        }).done(function (response) {
+            ToDoList.getItems();
+        })
+    },
+
+    updateItem: function (itemId, done) {
+        $.ajax({
+            url: ToDoList.API_BASE_URL + "?id=" + itemId,
+            method: "PUT",
+            contentType: "application/json",
+            data: JSON.stringify({
+                done: done
+            })
+        }).done(function (response) {
+            ToDoList.getItems();
+        })
+    },
+
+    displayItems: function (items) {
         var tableBodyHtml = '';
 
         items.forEach(item => tableBodyHtml += ToDoList.getItemRow(item));
-        $(`#to-do-items-table tbody`).html(tableBodyHtml);
+
+        $('#to-do-items-table tbody').html(tableBodyHtml);
     },
 
-    getItemRow: function (item){
-        var formatedDate = new Date(...item.deadline).toLocaleDateString("en-US");
+    getItemRow: function (item) {
+        var formattedDate = new Date(...item.deadline).toLocaleDateString("en-US");
 
-      return `<tr>
-            <td>${item.description}</td>
-            <td>${formatedDate}</td>
-            <td><input type="checkbox" class="mark-done-checkbox" title="Completed"/></td>
-            <td><a href="#" class="delete-item fa fa-trash"></a></td>
-        </tr>`
+        // ternary operator
+        var checkedAttribute = item.done ? "checked" : "";
+
+        return `<tr>
+                <td>${item.description}</td>
+                <td>${formattedDate}</td>
+                <td><input type="checkbox" class="mark-done-checkbox" 
+                title="Completed" data-id="${item.id}" ${checkedAttribute}/></td>
+                
+                <td><a href="#" class="delete-item fa fa-trash" data-id="${item.id}"></a></td>
+            </tr>`
     },
 
-    //mai jos e pentru evenimente, clickuri si toate cele
     bindEvents: function () {
 
         $('#new-item-form').submit(function (event) {
@@ -61,8 +87,28 @@ window.ToDoList = {
 
             ToDoList.createItem();
         });
-    }
 
+        // using delegate because the element a.delete-item is dynamically injected
+        // after the page has been loaded
+        $('#to-do-items-table').delegate('.delete-item', 'click', function (event) {
+            event.preventDefault();
+
+            var itemId = $(this).data('id');
+
+            ToDoList.deleteItem(itemId);
+        });
+
+        $('#to-do-items-table').delegate('.mark-done-checkbox', 'change',
+            function (event) {
+                event.preventDefault();
+
+                var itemId = $(this).data('id');
+                var checkboxChecked = $(this).is(':checked');
+
+                ToDoList.updateItem(itemId, checkboxChecked);
+            });
+    }
 };
-ToDoList.getItems()
+
+ToDoList.getItems();
 ToDoList.bindEvents();
